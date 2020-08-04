cff768010a2d9a9a89079923496188d1dede6fde
Aron Wouters <firestark@mail.com>
2020-08-04 18:41:24 +0200

    Improved dialog

--- io/channels/http/elm/src/Material/Dialog.elm
+++ io/channels/http/elm/src/Material/Dialog.elm
@@ -3,6 +3,7 @@ module Material.Dialog exposing (dialog)
 import Element exposing (Color, Element, centerX, centerY, column, el, fill, fromRgb, height, mouseOver, paddingXY, paragraph, px, rgb255, row, text, toRgb, width)
 import Element.Background as Background
 import Element.Border as Border
+import Element.Events exposing (onClick)
 import Element.Font as Font
 import Element.Input as Input
 import Material.Elevation as Elevation
@@ -11,40 +12,54 @@ import Theme exposing (Theme)
 
 dialog : Theme -> msg -> String -> String -> Element msg
 dialog theme msg title body =
+    column
+        [ width fill
+        , height fill
+        , Element.inFront <| scrim theme msg
+        , Element.inFront <| content theme msg title body
+        ]
+        []
+
+
+content : Theme -> msg -> String -> String -> Element msg
+content theme msg title body =
+    column
+        [ width (px 330)
+        , Background.color theme.surface
+        , centerX
+        , centerY
+        , Elevation.z4
+        ]
+        [ row
+            [ height (px 140)
+            , width fill
+            , Background.image "/assets/images/material.jpg"
+            ]
+            [ el
+                [ Font.size 24
+                , paddingXY 24 24
+                , Font.color (rgb255 255 255 255)
+                , Element.alignBottom
+                ]
+              <|
+                text title
+            ]
+        , row [ Font.color (Theme.highlight theme.kind 0.54) ]
+            [ paragraph [ Font.size 16, paddingXY 24 24 ]
+                [ text body
+                ]
+            ]
+        , buttons msg theme
+        ]
+
+
+scrim : Theme -> msg -> Element msg
+scrim theme msg =
     row
         [ width fill
         , height fill
         , Background.color (Theme.highlight theme.kind 0.38)
-        , Element.inFront <|
-            column
-                [ width (px 330)
-                , height (px 324)
-                , Background.color theme.surface
-                , centerX
-                , centerY
-                , Elevation.z4
-                ]
-                [ row
-                    [ height (px 140)
-                    , width fill
-                    , Background.image "/assets/images/material.jpg"
-                    ]
-                    [ el
-                        [ Font.size 24
-                        , paddingXY 24 24
-                        , Font.color (rgb255 255 255 255)
-                        , Element.alignBottom
-                        ]
-                      <|
-                        text title
-                    ]
-                , row [ Font.color (Theme.highlight theme.kind 0.54) ]
-                    [ paragraph [ Font.size 16, paddingXY 24 24 ]
-                        [ text body
-                        ]
-                    ]
-                , buttons msg theme
-                ]
+        , onClick msg
         ]
         []
 
